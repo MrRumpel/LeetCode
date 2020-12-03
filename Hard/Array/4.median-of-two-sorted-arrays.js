aba6b34849eccd48990dc38ab5db7a3c4b528e76
Rumpel <hades137747575@gmail.com>
2020-12-03 14:12:14 +0800

    小错误不断

--- Hard/Array/4.median-of-two-sorted-arrays.js
+++ Hard/Array/4.median-of-two-sorted-arrays.js
@@ -14,7 +14,19 @@ var findMedianSortedArrays = function (nums1, nums2) {
   const m = nums1.length;
   const n = nums2.length;
   if (m + n === 1) {
-    return n === 0 ? nums1[1] : nums2[1];
+    return n === 0 ? nums1[0] : nums2[0];
+  }
+  console.log(m, n);
+  const nums = [...nums1, ...nums2].sort((a, b) => a - b);
+  const l = nums.length;
+  // 余数为零 偶数 非零 奇数
+  const pos = Math.round(l / 2);
+  console.log(l);
+  if (l % 2 === 0) {
+    const answer = nums[pos] + nums[pos - 1];
+    return answer / 2;
+  } else {
+    return nums[pos - 1];
   }
 };
 // @lc code=end
