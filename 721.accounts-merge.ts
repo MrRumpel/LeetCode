8726462cd00b1852c108b64d9d9903afa0a83b13
Rumpel <hades137747575@gmail.com>
2020-12-04 16:19:39 +0800

    先拉平数组

---
+++ 721.accounts-merge.ts
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=721 lang=typescript
+ *
+ * [721] Accounts Merge
+ */
+
+// @lc code=start
+function accountsMerge(accounts: string[][]): string[][] {
+  const list = Array.from(new Set(accounts.flat()));
+  return [];
+};
+// @lc code=end
+
