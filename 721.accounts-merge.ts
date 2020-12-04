4ef64ff8477e426fb169fc8dd81f5944961d9700
Rumpel <hades137747575@gmail.com>
2020-12-04 16:58:30 +0800

    理解错题目 回头重做

--- 721.accounts-merge.ts
+++ 721.accounts-merge.ts
@@ -5,9 +5,25 @@
  */
 
 // @lc code=start
+
+function flat(arr: any[]) {
+  const res: any[] = [];
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      res.push(...flat(item))
+    } else {
+      res.push(item);
+    }
+  }
+  return res;
+}
 function accountsMerge(accounts: string[][]): string[][] {
-  const list = Array.from(new Set(accounts.flat()));
-  return [];
+  const list = Array.from(new Set(flat(accounts)));
+  const names = list.filter((item) => !item.includes('@'));
+  const mails = list.filter((item) => item.includes('@'));
+  return [...names.map((name) => ([name,
+    ...mails.filter((mail) =>
+      mail.split('@')[0].toLowerCase().includes(name.toLowerCase()))]))];
 };
 // @lc code=end
 
