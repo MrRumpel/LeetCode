6d149093dbe19a58fdd3f8e4a0a8719918c9e764
Rumpel <hades137747575@gmail.com>
2020-12-07 13:49:39 +0800

    暂时想不明白换题目

--- 8.string-to-integer-atoi.ts
+++ 8.string-to-integer-atoi.ts
@@ -6,10 +6,20 @@
 
 // @lc code=start
 function myAtoi(s: string): number {
-  const num = s.replace(/[a-zA-Z]/g, '').replace(/ /g, '');
+  if (s.match(/^[a-zA-Z]/)) {
+    return 0;
+  }
+  const num = s.replace(/[a-zA-Z]/g, '@@@').replace(/ /g, '').replace('-00', '-');
   console.info(s);
+  console.info(num.match(/^(\-?|\+?)\d+(\.\d+)?$/));
   console.info(num);
   console.info(Number(num));
+  if (num.indexOf('@@@') > 0) {
+    const temp = num.split('@@@')[0];
+    console.info(temp);
+    console.info(temp.match(/^(\-?)\d+(\.\d+)?$/));
+    return temp.match(/^(\-?)\d+(\.\d+)?$/) ? Number(temp) : 0;
+  }
   if (Number(num) < 2 ** 31 * -1) {
     return -2147483648;
   }
@@ -19,7 +29,7 @@ function myAtoi(s: string): number {
   if (Number.isNaN(Number(num))) {
     return 0;
   }
-  return s.match(/^[a-zA-Z]/) ? 0 : Number(num);
+  return Number(num);
 };
 // @lc code=end
 
