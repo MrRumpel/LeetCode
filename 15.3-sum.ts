74e68cdbd3c016a07f3e9e9fc9f9c6126a472140
Rumpel <hades137747575@gmail.com>
2020-12-08 14:20:11 +0800

    长度小于三直接排除

---
+++ 15.3-sum.ts
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=15 lang=typescript
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+function threeSum(nums: number[]): number[][] {
+  if (nums.length < 3) {
+    return [];
+  }
+  const list = nums.sort((a, b) => a - b);
+  return [];
+};
+// @lc code=end
+
