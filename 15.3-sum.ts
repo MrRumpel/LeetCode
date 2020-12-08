8d203754b91d35a733d389d1f98b4098b721e34e
Rumpel <hades137747575@gmail.com>
2020-12-08 16:51:56 +0800

    判断直接返回的情况

--- 15.3-sum.ts
+++ 15.3-sum.ts
@@ -6,11 +6,20 @@
 
 // @lc code=start
 function threeSum(nums: number[]): number[][] {
+  // 数组小于3直接返回空数组
   if (nums.length < 3) {
     return [];
   }
   const list = nums.sort((a, b) => a - b);
-  return [];
+  // 排序后 数组成员 全大于0 或者 全小于0 说明无解 直接返回空数组
+  if (list[0] > 0 || list[list.length - 1] < 0) {
+    return [];
+  }
+  const result: number[][] = [];
+  for (let i = 0; i < list.length - 2 && list[i] <= 0; i++) {
+
+  }
+  return result;
 };
 // @lc code=end
 
